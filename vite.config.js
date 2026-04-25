3e601282f5e27ea7d74e594d8cb0585a8a411bb7
LuizCoppini <coppiniluizcoppini@gmail.com>
2026-04-24 21:02:52 -0300

    fix: update base path for github pages

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,6 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  // Se seu repositório for github.com/LuizCoppini/portfolio, defina o base:
-  // base: "/portfolio/",
-  // Se for github.com/LuizCoppini/LuizCoppini.github.io, deixe como "/":
-  base: "/",
+  // Ajustado para o nome do seu repositório no GitHub
+  base: "/portfolio/",
 });
